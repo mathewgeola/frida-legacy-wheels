0cf0e7de19f2c49267db1833f90fe17c5a929f9e
mathewgeola <mathewgeola@gmail.com>
2026-01-29 21:11:29 +0800

    2026/01/29 21:11

--- 12.8.0/macosx.py
+++ 12.8.0/macosx.py
@@ -7,13 +7,13 @@ pyenv local 3.6.15
 
 python macosx.py
 
-pip install frida-12.8.0-cp36-cp36m-macosx_10_9_x86_64.whl --force-reinstall
+pip install frida-12.8.0-cp36-cp36m-macosx_10_6_x86_64.whl --force-reinstall
 """
 
 import zipfile
 
 egg_file = "frida-12.8.0-py3.6-macosx-10.6-intel.egg"
-wheel_file = "frida-12.8.0-cp36-cp36m-macosx_10_9_x86_64.whl"
+wheel_file = "frida-12.8.0-cp36-cp36m-macosx_10_6_x86_64.whl"
 
 dist_info = "frida-12.8.0.dist-info"
 
@@ -21,7 +21,7 @@ with zipfile.ZipFile(egg_file, "r") as egg, zipfile.ZipFile(wheel_file, "w", zip
     for item in egg.infolist():
         whl.writestr(item, egg.read(item.filename))
 
-    whl.writestr(f"{dist_info}/WHEEL", "Wheel-Version: 1.0\nTag: cp36-cp36m-macosx_10_9_x86_64\n")
+    whl.writestr(f"{dist_info}/WHEEL", "Wheel-Version: 1.0\nTag: cp36-cp36m-macosx_10_6_x86_64\n")
     whl.writestr(f"{dist_info}/METADATA", "Metadata-Version: 2.1\nName: frida\nVersion: 12.8.0\n")
     whl.writestr(f"{dist_info}/RECORD", "")
 
